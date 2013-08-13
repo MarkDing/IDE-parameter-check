e4fe2bca104175be4eb351ce1381c36ab1b500ad
Mark <xiaoxid@163.com>
2013-08-13 18:38:43 +0800

    Add copy head files into current directory function.

--- checkfile.rb
+++ checkfile.rb
@@ -31,34 +31,62 @@ class SilabsWSP
 	@@restore_default_setting = 0
 	
 	@@headfile_dir = "C:/Silabs/MCU/INC"
-	@@headfiles = [
-	"/compiler_defs.h",
-	"/C8051F500_defs.h"
-	]
 	
 	def initialize(path)
 		@path = path
 	end
 
+	def find_mcu_headfile(cur_dir)
+		Find.find(cur_dir) do |fn|
+			ext_name = File.extname(fn)
+			if ext_name == ".c" or ext_name == ".h"
+				f = File.open(fn, "r+")
+				lines = f.readlines
+				lines.each do |line|
+					if line.include?"#include"
+						a = /[cC]8051[fF]\d{3}.[hH]/.match(line)
+						a = /[cC]8051[fF]\d{3}_[dD][eE][fF][sS].[hH]/.match(line) if not a 
+						if a 
+							headfile = "/" + a[0]
+							dst = cur_dir + headfile 
+							src = @@headfile_dir + headfile
+							if not File.exist?(dst) 
+								FileUtils.cp(src, dst)
+								p "Copy #{a[0]} to " + dst
+							end
+							f.close
+							return
+						end
+					end
+				end
+				f.close
+			end
+		end
+	end
+	
 	def copy_head_files(fn)
 		cur_dir = File.dirname(fn)
 		# compiler_defs.h
-		dst = cur_dir + @@headfiles[0] 
-		src = @@headfile_dir + @@headfiles[0]
+		dst = cur_dir + "/compiler_defs.h" 
+		src = @@headfile_dir + "/compiler_defs.h"
 		if not File.exist?(dst) 
-			#FileUtils.cp(src, dst)
+			FileUtils.cp(src, dst)
 			p "Copy compiler_defs.h to " + dst
 		end
-		# C8051Fxx_defs.h
+		# C8051Fxxx_defs.h or C8051Fxxx.h
+		find_mcu_headfile(cur_dir)
 	end
 	
 	def handle_contents(line, a)
 		new_line = line
-		if @@restore_default_setting == 0
+		if @@restore_default_setting == 1
+#			new_line = a + @@key_contents[a]
+		else
 			case a
 			when "Vendor="
 				p "The Tool Definition Presets is selecting preset"
 				@@restore_default_setting = 1
+				#new_line = a + @@key_contents[a]
 			when "AssFlag=", "CompFlag="
 				if line.include?" INCDIR"
 					tmp = line.split(" ")
@@ -99,11 +127,12 @@ class SilabsWSP
 			i += 1
 		end
 		f.close
+		
 		copy_head_files(fn)
 		if @@restore_default_setting == 0
-			#f = File.new(fn, "w+")
-			#f.puts lines
-			#f.close
+			f = File.new(fn, "w+")
+			f.puts lines
+			f.close
 		end
 		p "**********************************************************************"
 	end
